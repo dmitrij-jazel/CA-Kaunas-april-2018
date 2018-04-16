0f973eb744076f9f105fe8c9d6993f2ef17a72a6
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-04-16 13:11:43 +0300

    Adding more JS

--- JS Arrays/js/script.js
+++ JS Arrays/js/script.js
@@ -3,23 +3,34 @@ let cities = []; // kuriam nauja masiva
 cities.push("Jonava");
 cities.push("Vilnius");
 cities.push("Kaunas");
-cities.push("Palanga");
-cities.push("Klaipeda");
-cities.push("Utena");
-cities.push("Ignalina");
-cities.push("Taurage");
-cities.push("Panevezys");
-cities.push("Siauliai");
-cities.push("Kazlu ruda");
+cities.push("Naujoji,Akmene");
 
+
+
+
+
+console.log(cities);
+console.log(cities.toString());
 
 
 function test(){
-
+    
     console.log(typeof cities); // tikrinam masivo tipa
 
     let newCity = document.getElementById("txt_city").value; // nuskaitom nauja miesta is text laukelio
     cities.push(newCity); // pridedam nauja miesta i masiva
 
-    document.getElementById("result").innerText = cities;
+    //document.getElementById("result").innerText = cities;
+    printCities(cities); // su funkcijos kvietumu - siunciam kintamaji (masivas) "cities"    printCities(cities);
+}
+
+
+function printCities(cities){ // funkcijai reikalingas masivas "cities"
+    let h = "";
+    for(let i = 0; i < cities.length; i++){
+        console.log(i);
+        console.log(cities[i]);
+        h = h + "<div><h3 class='prettyText'>"+cities[i]+"</h3></div>"; // string aggregate
+    }
+    document.getElementById("result").innerHTML = h;
 }
