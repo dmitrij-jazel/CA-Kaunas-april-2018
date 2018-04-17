d03376f3c2980379648e39cea13469a95d7e01c0
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-04-17 09:54:42 +0300

    Shopping list advanced

--- ShoppingList (Advanced)/js/script.js
+++ ShoppingList (Advanced)/js/script.js
@@ -1,22 +1,19 @@
 let product_array = [];
 
-function sub(){
+function add(){
     //nuskaityti reiksme nuo laukelio
     let newProduct = document.getElementById("prod").value;
 
     //patikrinti ar elementas egzistuoja
-    let egzistuoja = false; // boolean
+    let rasta_dublikatu = false; // boolean
+    
+    rasta_dublikatu = checkForDuplicates(newProduct, product_array);
 
-    egzistuoja = checkForDuplicates(newProduct, product_array);
-
-    // issaugoti reiksme i masyva
-    if(egzistuoja){
-        // show alert - product exists
-        alert("DUPLIKATAS");
-    }else{
-        // add product that does not exist
-        product_array.push(newProduct); 
-    }     
+    if (rasta_dublikatu  ==  false) {
+        product_array.push(newProduct);
+    } else {
+        alert("Reiksme jau egzistuoja!");
+    }   
    
     // atvaizduoti masyva
     printProducts(product_array);
@@ -36,10 +33,12 @@ function printProducts(product_array){
 
 function checkForDuplicates(newProduct, product_array){
     let found = false;
+    
     for(let i = 0; i < product_array.length; i++){
         if(product_array[i] == newProduct){
             found = true;
-        }
+        }        
     }
+
     return found;
 }
