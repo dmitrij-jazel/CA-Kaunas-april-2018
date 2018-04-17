0a6db685824b39f2603e5418f9e902ffaf7927ab
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-04-17 15:50:21 +0300

    Ikelem CitySearch

--- CitySearch/js/script.js
+++ CitySearch/js/script.js
@@ -1,6 +1,23 @@
-let miestai = ["New york", "Berlin","Oslo","Riga","Moscow","Rome",""];
+let miestai = ["New york", "Berlin","Oslo","Riga","Moscow","Rome"];
 
 
 function check(){
-    alert("Implement here");
+    //Step1: turim nuskaityti teksta is laukelio (Input)
+    let city = document.getElementById("city").value;
+    console.log(city); // Doublecheck ar tikrai nuskaitom taisinga reiksme
+
+
+
+    //Step2: patikrinti ar tekstas yra masyve (Logic)
+    let cityFound = ""; // bus naudojamas rezultatui atvaizduoti
+    if(miestai.indexOf(city) > -1){
+        // toks miestas yra
+        cityFound = "Toks miestas yra";
+    }else{
+        // tokio miesto nera
+        cityFound = "Tokio miesto nera";
+    }
+    
+    //Step3: atvaizduoti rezultata (Output)
+    document.getElementById("result").innerHTML = "<h3>"+cityFound+"</h3>";
 }
