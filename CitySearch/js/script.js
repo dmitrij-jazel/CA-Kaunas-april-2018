1499b4372d48a279e2a5ecaea9167dd8b2007be6
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-04-17 15:17:25 +0300

    Adding City Search

---
+++ CitySearch/js/script.js
@@ -0,0 +1,6 @@
+let miestai = ["New york", "Berlin","Oslo","Riga","Moscow","Rome",""];
+
+
+function check(){
+    alert("Implement here");
+}
--- Duplicate check/js/script.js
+++ Duplicate check/js/script.js
@@ -1,18 +1,32 @@
 let colors = ["red","green","blue"];
 colors.push("red");
-
 colors = clearDuplicates(colors);
 
 let colors_as_string = colors.toString();
-console.log(typeof colors_as_string); // Object
-console.log(colors_as_string);
+// sukuria stringa is masyvo "red,green,blue"
 
 document.getElementById("result").innerText = colors_as_string;
 //console.log(colors);
 
 
+let split_list = "black,blue,yellow";
+let splitted_colors = split_list.split(',');
+
+splitted_colors = splitted_colors.reverse(); // Apvercia masyva, padaro "yellow,blue,black" 
+
+// tikrinam ar zodis yra Rastas
+let wordFound = splitted_colors.indexOf("black") > -1;
+
+console.log(splitted_colors);
 
 
+let color_list = "black,blue,yellow".split(',');
+//console.log(colors2);
+if(color_list.indexOf("red") == -1){
+    //console.log("red nerasta");
+}else{
+    //console.log("Blue gyvena "+colors2.indexOf("blue")+" vagone");
+}
 
 
 
@@ -47,11 +61,12 @@ function clearDuplicates(list_A){
 
 //pop
 //splice()
-
+//split()
 //slice()
 //indexOf()
 //toString()
-//reverse()
 
-//sort()
+
+
+
 
