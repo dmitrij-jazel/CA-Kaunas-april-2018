d0054b5f919cb6a1deaa93eeda686054058defb9
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-04-18 15:32:43 +0300

    Issaugom Shopping list i sarasa

--- ShoppingList (Advanced)/js/script.js
+++ ShoppingList (Advanced)/js/script.js
@@ -1,5 +1,16 @@
 let product_array = [];
 
+
+let stored_products_string = localStorage.getItem("PRODUCTS");
+let stored_products = JSON.parse(stored_products_string);
+console.log(stored_products);
+
+if(stored_products != null){    
+    product_array = stored_products;
+}
+
+printProducts(product_array);
+
 function add(){
     //nuskaityti reiksme nuo laukelio
     let newProduct = document.getElementById("prod").value;
@@ -10,13 +21,17 @@ function add(){
     rasta_dublikatu = checkForDuplicates(newProduct, product_array);
 
     if (rasta_dublikatu  ==  false) {
-        product_array.push(newProduct);
+        product_array.push(newProduct);        
     } else {
         alert("Reiksme jau egzistuoja!");
     }   
    
     // atvaizduoti masyva
     printProducts(product_array);
+
+    // issaugot shopping lista
+    let product_array_as_string = JSON.stringify(product_array);
+    localStorage.setItem("PRODUCTS", product_array_as_string);
 }
 
 function printProducts(product_array){
