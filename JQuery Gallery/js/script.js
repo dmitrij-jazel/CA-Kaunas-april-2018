2723504db4aa202c12a528e187739f554b660e0e
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-04-20 10:03:29 +0300

    adding jquery

---
+++ JQuery Gallery/js/script.js
@@ -0,0 +1,11 @@
+// function btn_click(btn){    
+//     let btnID = btn.id;
+
+//     alert(btnID+" was clicked");
+//     //document.getElementById("").innerHTML = "<h3>"+btnID+" was clicked</h3>";
+// }
+
+//"CLICK" Event listener
+$(".btn").click(function () {
+    alert("btn was clicked");
+});
