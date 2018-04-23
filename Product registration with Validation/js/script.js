0465d2c9e6fe4b8d60c2e583ee196c5dc0c11637
DMIJA-PC <DMIJA-PC@DESKTOP-HENVI18>
2018-04-23 14:50:53 +0300

    Adding additional feedback to product registration

--- Product registration with Validation/js/script.js
+++ Product registration with Validation/js/script.js
@@ -23,6 +23,25 @@ function add(){
 
 	let errors_found = false;
 
+	// Tikrinam "Pavadinimas" laukeli
+	if(pavadinimas_input){
+		// Laukelis nera tuscias
+		if(pavadinimas_input.length > 50){
+			$("#pavadinimas-validation-error").text("Per ilgas prekes pavadinimas.");
+			$("#pavadinimas-validation-error").fadeIn('fast');		
+			$("#pavadinimas").addClass("is-invalid");
+		}else{
+			// klaidu nerasta, viskas gerai
+			$("#pavadinimas-validation-error").fadeOut('fast');
+			$("#pavadinimas").removeClass("is-invalid");
+		}
+	}else{
+		$("#pavadinimas-validation-error").text("Pavadinimas yra privalomas");
+		$("#pavadinimas-validation-error").fadeIn('fast');		
+		$("#pavadinimas").addClass("is-invalid");
+		errors_found = true;
+	}
+
 	// Tikrinam "Kaina" laukeli
 	if(kaina_input){
 		// Laukelis nera tuscias
@@ -30,36 +49,31 @@ function add(){
 		// patikrinti ar klaina turi "+" ARBA "-"
 		if(kaina_input.indexOf('+') > -1 || kaina_input.indexOf('-') > -1){
 			// rastas neleistinas "+" zenklas - klaida
-			alert("Demesio rastas + arba -");
+			$("#kaina-validation-error").text("Demesio rastas + arba -"); //cia yra pranesimo zinute
+			$("#kaina-validation-error").fadeIn('fast'); // cia mes parodom pati pranesima
+			$("#kaina").addClass("is-invalid"); // cia mes pridedam raudona remeli teksto laukeliui
 			errors_found = true;
 		}else{
 			if(Number(kaina_input) > 0 && Number(kaina_input) < 10000){
 				// klaidu nerasta, viskas gerai
+				$("#kaina-validation-error").fadeOut('fast');
+				$("#kaina").removeClass("is-invalid");
 			}else{
-				alert("Demesio, kaina privalo buti tarp 0 - 10000");
-				errors_found = true;
+				$("#kaina-validation-error").text("Demesio, kaina privalo buti tarp 0 - 10000"); //cia yra pranesimo zinute
+				$("#kaina-validation-error").fadeIn('fast'); // cia mes parodom pati pranesima
+				$("#kaina").addClass("is-invalid"); // cia mes pridedam raudona remeli teksto laukeliui
+				errors_found = true;			
 			}
 		}
 	}else{
 		// KLAIDA: "Kaina" laukelis yra privalomas ir negali buti tuscias
-		alert("Kaina yra provaloma");
+		$("#kaina-validation-error").text("Kaina yra provaloma"); //cia yra pranesimo zinute
+		$("#kaina-validation-error").fadeIn('fast'); // cia mes parodom pati pranesima
+		$("#kaina").addClass("is-invalid"); // cia mes pridedam raudona remeli teksto laukeliui
 		errors_found = true;
 	}
 
-	// Tikrinam "Pavadinimas" laukeli
-	if(pavadinimas_input){
-		// Laukelis nera tuscias
-		if(pavadinimas_input.length > 50){
-			// cia yra klaida, perilgas produkto pavadinimas
-			alert("Demesio, per ilgas prekes pavadinimas");			
-			errors_found = true;
-		}else{
-			// klaidu nerasta, viskas gerai
-		}
-	}else{
-		alert("Pavadinimas yra provalomas");
-		errors_found = true;
-	}
+	
 
 	
 
@@ -71,6 +85,7 @@ function add(){
 		if(kiekis_input.indexOf('+') > -1 || kiekis_input.indexOf('-') > -1){
 			// rastas neleistinas "+" zenklas - klaida
 			$("#kiekis-validation-error").text("Demesio rastas + arba -"); //cia yra pranesimo zinute
+
 			$("#kiekis-validation-error").fadeIn('fast'); // cia mes parodom pati pranesima
 			$("#kiekis").addClass("is-invalid"); // cia mes pridedam raudona remeli teksto laukeliui
 			errors_found = true;			
@@ -114,7 +129,7 @@ function add(){
 		errors_found = true;
 	}
 
-
+	// patikrinimas, ar testi registracija ar roditi errora
 	if(!errors_found){
 		// Jeigu klaidu nera, viskas gerai - galima registruot nauja produkta.
 		//Logic
@@ -136,11 +151,10 @@ function add(){
 
 		//Isvalyti forma
 		resetForm();
+		$('#reg_ok').removeClass("fade");
 	}else{
-		
-	}
-
-	
+		$('#reg_error').removeClass("fade");
+	}	
 }
 
 function printProduktai(produktai){
